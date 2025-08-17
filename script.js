0c3901ac8fd7188b81a994172522513c9bd5d84e
jecklzh <40534273+jecklzh@users.noreply.github.com>
2025-08-17 13:32:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const chatMessages = document.getElementById('chat-messages');
 const chatInput = document.getElementById('chat-input');
 const sendButton = document.getElementById('send-button');
 
-const API_ENDPOINT = 'https://arelia-worker.578360521.workers.dev'; 
+const API_ENDPOINT = 'https://arliaapi.stevel.eu.org'; 
 
 const videoLibrary = {
     hello: ['videos/greeting.mp4'],
